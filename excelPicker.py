0ac11a7f50fad2d2d269dcc0b4fa56e66e67f328
Finn McKinley <106862959+FinnMcKinley@users.noreply.github.com>
2022-06-07 17:12:55 +1200

    Update excelPicker.py

--- excelPicker.py
+++ excelPicker.py
@@ -1,13 +1,20 @@
 import random
 from datetime import date
 from openpyxl import load_workbook
+from openpyxl import Workbook
 
 today = date.today()
 dateToday = today.strftime("%d/%m/%y")
-randomNumber = random.randint(0,750)
+randomNumber = random.randint(2,749)
 print(randomNumber)
 print(dateToday)
+randomCell = 'A' + str(randomNumber)
+print('randomCell is:')
+print(randomCell)
 
-wb = load_workbook(filename= 'ExcelSheet.xlsx')
-ws = wb.active
-print(ws['A1'].value)
+InfoWB = load_workbook(filename= 'ExcelSheet.xlsx')
+InfoWS = InfoWB.active
+
+dbWB = load_workbook(filename= 'StockPicks.xlsx')
+db = dbWB.active
+dbWB.save('StockPicks.xlsx')
