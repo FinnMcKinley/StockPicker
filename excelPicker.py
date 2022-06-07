7f2aa4c8abf542a85b107d8f041b14b9409bc072
Finn McKinley <106862959+FinnMcKinley@users.noreply.github.com>
2022-06-07 16:13:15 +1200

    Update excelPicker.py
    Added ability to read an excel sheet using openpyxl

--- excelPicker.py
+++ excelPicker.py
@@ -1,8 +1,13 @@
 import random
 from datetime import date
+from openpyxl import load_workbook
 
 today = date.today()
 dateToday = today.strftime("%d/%m/%y")
 randomNumber = random.randint(0,750)
 print(randomNumber)
 print(dateToday)
+
+wb = load_workbook(filename= 'ExcelSheet.xlsx')
+ws = wb.active
+print(ws['A1'].value)
