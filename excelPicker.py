e3e7db18c054ba37ccd2fc789d0bebc763c7b484
Finn McKinley <106862959+FinnMcKinley@users.noreply.github.com>
2022-06-07 17:57:18 +1200

    Updated. (from line 22 does not work)

--- excelPicker.py
+++ excelPicker.py
@@ -8,13 +8,18 @@ dateToday = today.strftime("%d/%m/%y")
 randomNumber = random.randint(2,749)
 print(randomNumber)
 print(dateToday)
-randomCell = 'A' + str(randomNumber)
+randomCell = 'H' + str(randomNumber)
 print('randomCell is:')
 print(randomCell)
 
 InfoWB = load_workbook(filename= 'ExcelSheet.xlsx')
 InfoWS = InfoWB.active
+randomStock = InfoWS[randomCell].value
+print(randomStock)
 
 dbWB = load_workbook(filename= 'StockPicks.xlsx')
 db = dbWB.active
+# This next bit doesn't work either but I haven't researched or tried fixing it yet
+# dbWB['A2'] = randomStock
+# dbWB.__setattr__('A2', randomStock)
 dbWB.save('StockPicks.xlsx')
